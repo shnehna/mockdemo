524ffbd3507ee3b59044c1bf01120f8abf8fd9de
shenhan <mr_shenhan@outlook.com>
2020-01-16 16:42:18 +0800

    接口是用来定义类型的，类用来定义常量

---
+++ src/main/java/org/example/power/effectivejava/PhysicalConstants.java
@@ -0,0 +1,15 @@
+package org.example.power.effectivejava;
+
+
+/**
+ * @author shenhan
+ * @create 2020/1/16 15:34
+ * @description
+ */
+public class PhysicalConstants {
+    private PhysicalConstants() {
+    }
+
+    private static final double AVOGADROS_NUMBER = 6.0221123;
+    private static final double PI = Math.PI;
+}
